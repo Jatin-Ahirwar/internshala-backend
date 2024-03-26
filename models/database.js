21147fe87b208622e82bce465977c23a7a6297f1
Jatin-Ahirwar <jatinahirwar24@gmail.com>
2024-03-26 14:00:27 +0530

    connection change

--- models/database.js
+++ models/database.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose')
 exports.connectDatabase = async () =>{
     try {
         await mongoose.connect(process.env.MONGODBD_URL , {
-            useNewUrlParser:true,
-            useUnifiedTopology:true,
-            useCreateIndex:true}
+
+            useNewUrlParser: true, 
+
+            useUnifiedTopology: true         }
         ) 
         console.log("Database Connected")
     } catch (error) {
