928923bf24a2a36b3b1e0f83c6018a3b43bdf98a
Jatin-Ahirwar <jatinahirwar24@gmail.com>
2025-03-11 17:22:59 +0530

    changes in the cookie

--- utils/SendToken.js
+++ utils/SendToken.js
@@ -6,7 +6,7 @@ exports.sendtoken = (student,statuscode,res) =>{
             Date.now() + process.env.COOKIE_EXPIRE *24 * 60 * 60 * 1000
         ),
         httpOnly:true,
-        secure:false
+        secure:process.env.SECURE
 
     }
     res.status(statuscode)
